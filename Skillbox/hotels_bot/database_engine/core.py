bc15ee46afc32a6875206d3bc8393dc1a1573e2b
Igor Pozdnyakov <hharrys@vk.com>
2023-08-10 16:28:45 +0400

    добавил поля для модели таблицы истории

--- Skillbox/hotels_bot/database_engine/core.py
+++ Skillbox/hotels_bot/database_engine/core.py
@@ -16,27 +16,30 @@ class Users(Model):
 class History(Model):
     user = ForeignKeyField(Users, related_name='')
     link = CharField()
+    info = CharField()
+    date = DateField()
 
     class Meta:
         database = db
 
 
-# # создаем реальную таблицу на основе модели, которая описана в классе
-# Users.create_table()
-# History.create_table()
-#
-# # создаем записи (строки)
-# sergey = Users.create(name='Sergey', nickname='@creespy')
-# igor = Users.create(name='Igor', nickname='@harry')
-#
-# link_1 = History.create(user=sergey, link='hilton.com')
-# link_2 = History.create(user=igor, link='mariot.com')
-# link_3 = History.create(user=igor, link='radison.com')
+# создаем реальную таблицу на основе модели, которая описана в классе
+Users.create_table()
+History.create_table()
+
+# создаем записи (строки)
+sergey = Users.create(name='Sergey', nickname='@creespy')
+igor = Users.create(name='Igor', nickname='@harry')
+
+link_1 = History.create(user=sergey, link='hilton.com')
+link_2 = History.create(user=igor, link='mariot.com')
+link_3 = History.create(user=igor, link='radison.com')
 
 # Сергей создал новый запрос
 link_4 = History.create(
     user=Users.select().where(Users.name == 'Sergey').get(),
     link='google.com')
+
 # TODO добавить проверку на существование юзера в БД, чтобы не было дублей
 # TODO добавить методы добавления истории и новых юзеров, чтобы в боте было минимум действий с БД и все выполнялось одной строкой
 
