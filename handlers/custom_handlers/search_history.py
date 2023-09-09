c9e78a5924e34ea6b07488025cb66fa4f1a5d5c2
igor pozdniakov <hharrys@vk.com>
2023-09-09 21:13:12 +0300

    поправил метод show_request_history; пользователю выдает историю поиска в более читабельном виде(сообщение с текстом запроса+инлайн-кнопка)

--- handlers/custom_handlers/search_history.py
+++ handlers/custom_handlers/search_history.py
@@ -9,7 +9,8 @@ from loader import bot
 @bot.message_handler(commands=["history"])
 def get_history(message: Message) -> None:
     """
-
+    Метод для получения истории. При вызове команды выдается история последних 10 запросов с инлайн-кнопкой,
+    которая выводит информацию о найденных билетах
     :param message: Message
     :return: None
     """
