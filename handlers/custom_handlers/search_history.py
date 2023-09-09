e6632179f69d946eb01b350a958c600a94f1fa3b
igor pozdniakov <hharrys@vk.com>
2023-09-09 21:08:49 +0300

    поправил метод show_request_history; пользователю выдает историю поиска в более читабельном виде(сообщение с текстом запроса+инлайн-кнопка)

--- handlers/custom_handlers/search_history.py
+++ handlers/custom_handlers/search_history.py
@@ -1,6 +1,8 @@
 from telebot.types import Message
 
-from keyboards.inline.search_history_keyboards.search_history_keyboard import show_request_history
+from keyboards.inline.search_history_keyboards.search_history_keyboard import (
+    show_request_history,
+)
 from loader import bot
 
 
@@ -11,7 +13,5 @@ def get_history(message: Message) -> None:
     :param message: Message
     :return: None
     """
-    bot.send_message(
-        message.from_user.id,
-        "Выберите один из недавних запросов:")
+    bot.send_message(message.from_user.id, "Выберите один из недавних запросов:")
     show_request_history(message)
