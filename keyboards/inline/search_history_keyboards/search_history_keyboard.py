f449f9d81d7508a7c31a7ac490769dfff5f57c94
igor pozdniakov <hharrys@vk.com>
2023-09-10 15:47:29 +0300

    работа над получением информации о найденных билетах из таблицы TicketInfo, пока удалось добиться вывода ответа только в консоли, т к для callback'a инлайн-кнопки слишком большой размер

--- keyboards/inline/search_history_keyboards/search_history_keyboard.py
+++ keyboards/inline/search_history_keyboards/search_history_keyboard.py
@@ -1,6 +1,6 @@
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
-from database.db_core import History, Users
+from database.db_core import History, Users, TicketsInfo, db
 from loader import bot
 
 
@@ -12,13 +12,30 @@ def show_request_history(message: Message):
         .where(History.user == Users.get(Users.nickname == message.chat.username))
         .order_by(History.date_time.desc())
     ):
-        history_entry = f"Команда: \\{history_entry.command};\nЗапрос: {history_entry.request_info};\nДата и время запроса: {history_entry.date_time}"
-        bot.send_message(
-            message.from_user.id,
-            history_entry,
-            reply_markup=InlineKeyboardMarkup().add(
-                InlineKeyboardButton(
-                    text="вывести результат поиска", callback_data="дописать"
-                )
-            ),
-        )
+        history_entry_info = f"Команда: \\{history_entry.command};\nЗапрос: {history_entry.request_info};\nДата и время запроса: {history_entry.date_time}"
+        one_ticket_info_str = ''
+        for one_ticket_info in TicketsInfo.select().where(
+            TicketsInfo.request_id == history_entry.id
+
+        ):
+            one_ticket_info_str = (
+                f"{one_ticket_info.ticket_info}, {one_ticket_info.ticket_link}"
+            )
+            print(one_ticket_info_str)
+        # bot.send_message(
+        #     message.from_user.id,
+        #     history_entry_info,
+        #     reply_markup=InlineKeyboardMarkup().add(
+        #         InlineKeyboardButton(
+        #             text="вывести результат поиска", callback_data=one_ticket_info_str
+        #         )
+        #     ),
+        # )
+
+
+# for one_ticket_info in TicketsInfo.select().where(TicketsInfo.request_id == History.get()):
+#     one_ticket_info = f"{one_ticket_info.ticket_info}"
+#     print(one_ticket_info)
+#
+
+# print(History.get())  # сработало, когда прошел скрипт (выполнил команду в тг-боте)
